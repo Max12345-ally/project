6917143741b25e53c30c8ad366f8fe632a5907c4
Max12345-ally <ceo@rf.ru>
2024-10-05 23:14:19 +0300

    typescript in project added

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack')
 
 module.exports = {
     mode: "development",
-    entry: path.resolve(__dirname, 'src', 'index.js'),
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, 'build'),
@@ -14,5 +14,17 @@ module.exports = {
             template: path.resolve(__dirname, 'public', 'index.html')
         }),
         new webpack.ProgressPlugin(),
-    ]
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+          },
+        ],
+      },
+      resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+      },
 }
