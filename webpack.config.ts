d9c3dbdeb76fcd584bbf4cda2e92b3221ea3e148
Max12345-ally <ceo@rf.ru>
2024-10-09 23:40:15 +0300

    ts nodes configures

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,33 +1,36 @@
-const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-const webpack = require('webpack')
+import path from 'path'
+import webpack from 'webpack'
+import HTMLWebpackPlugin from'html-webpack-plugin'
 
-module.exports = {
-    mode: "development",
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    output: {
-        filename: "[name].[contenthash].js ",
-        path: path.resolve(__dirname, 'build'),
-        clean: true,
+const config: webpack.Configuration = {
+  mode: "development",
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  output: {
+      filename: "[name].[contenthash].js ",
+      path: path.resolve(__dirname, 'build'),
+      clean: true,
+  },
+
+
+  plugins: [
+      new HTMLWebpackPlugin({
+          template: path.resolve(__dirname, 'public', 'index.html')
+      }),
+      new webpack.ProgressPlugin(),
+  ],
+  module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
     },
-
-
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html')
-        }),
-        new webpack.ProgressPlugin(),
-    ],
-    module: {
-        rules: [
-          {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-          },
-        ],
-      },
-      resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-      },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
 }
+
+
+export default config
